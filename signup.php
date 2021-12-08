2fbf10f31e6f94f9b1534842309b2d69ce147a0a
LucasJC <lucasjc2208@gmail.com>
2021-12-08 15:53:25 +0100

    signup

---
+++ signup.php
@@ -0,0 +1,17 @@
+
+<section class="signup-form">
+<h2>Opret profil</h2>
+<form action="signup.inc.php" method="post">
+
+    <input type="text" name="name" placeholder="Full name...">
+    <input type="text" name="email" placeholder="Email...">
+    <input type="text" name="uid" placeholder="Username...">
+    <input type="password" name="pwd" placeholder="Password...">
+    <input type="password" name="pwdrepeat" placeholder="Repeat password...">
+    <button type="submit" name="submit">Opret profilen</button>
+
+</form>
+
+
+
+</section>
