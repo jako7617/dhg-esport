3d0defc2d9bbd108e517b176b4c2fc2294f29d2a
LucasJC <lucasjc2208@gmail.com>
2021-12-13 05:04:39 +0100

    update

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -12,14 +12,50 @@ function emptyInputSignup($name, $email, $pwd, $pwdRepeat) {
 
 }
 
-function emptyInputSignup($name, $email, $pwd, $pwdRepeat) {
+function invalidEmail($email) {
     $result;
-        if (empty($name) || empty($email) || empty($pwd) || empty($pwdRepeat)) {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $result = true;
         }
         else {
             $result = false;
         }
             return $result;
+
+}
+
+function pwdMatch($pwd, $pwdRepeat) {
+    $result;
+        if ($pwd !== $pwdRepeat)) {
+            $result = true;
+        }
+        else {
+            $result = false;
+        }
+            return $result;
+
+}
+
+function uidExists($conn, $name, $email) {
+    $sql = "SELECT * FROM users WHERE usersUid = ? OR usersEmail = ?;";
+    $stmt = mysqli_stmt_init($conn); 
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../signup.php?error=stmtfailed");
+        exit();
+    }
+
+    mysqli_stmt_bind_param($stmt, "ss", $email);
+    mysqli_stmt_execute($stmt);
+
+    $resultData = mysqli_stmt_get_result($stmt); 
+    
+    if (mysqli_fetch_assoc($resultData)) {
+
+    }
+
+    else {
+        $result = false;
+        return $result;
+    }
 
 }
